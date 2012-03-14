d9d580ce617e89a4147c80c9cee6eee60bd40870
Callie Johnston <calliej7@vt.edu>
2012-03-14 11:42:48 -0400

    Fixed errors so program now runs correctly

--- change_grade.py
+++ change_grade.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
 
-import re
 
 def changeGrade(file, searchString, newGrade, verbose):
 	for line in file:
 		m = re.search(searchString, line)
-		values = line.rstript().split(',')
+		values = line.rstrip().split(',')
 		if m:
 			if verbose:
 				stderr.write("%r\n" % values)
 			values[3] = newGrade
-	print ",".join(values)
+		print ",".join(values)
 
 if __name__=='__main__':
+	import re
 	import fileinput
 	from sys import stderr, stdout, argv, exit
-	verbose = false
+
+	verbose = False
+
 	if len(argv) != 3:
 		stderr.write("Usage: change_grade.py STRING VALUE \n")
 		exit (1)
