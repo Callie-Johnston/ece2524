7c4c95cfea1ec27d9ada879828fd92949314c1ac
Callie Johnston <calliej7@vt.edu>
2012-03-14 10:50:39 -0400

    Add function and main block

--- change_grade.py
+++ change_grade.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
 
 import re
-import fileinput
-from sys import stderr,stdout,argv,exit
 
-verbose=False
+def changeGrade(file, searchString, newGrade, verbose):
+	for line in file:
+		m = re.search(searchString, line)
+		values = line.rstript().split(',')
+		if m:
+			if verbose:
+				stderr.write("%r\n" % values)
+			values[3] = newGrade
+	print ",".join(values)
 
-if len(argv) != 3:
-    stderr.write("Usage: change_grade.py STRING VALUE\n")
-    exit(1)
-    
-for line in fileinput.input('-'):
-    m = re.search(argv[1], line)
-    values = line.rstrip().split(',') #note that split accepts an optional parameter to define what character the string is split on. If none is provided it defaults to any whitespace character
-    if m:
-        if verbose:
-            stderr.write("%r\n" % values)
-        values[3] = argv[2] 
-
-    print ",".join(values)
+if __name__=='__main__':
+	import fileinput
+	from sys import stderr, stdout, argv, exit
+	verbose = false
+	if len(argv) != 3:
+		stderr.write("Usage: change_grade.py STRING VALUE \n")
+		exit (1)
+	else:
+		changeGrade(fileinput.input('-'), argv[1], argv[2], verbose)	
